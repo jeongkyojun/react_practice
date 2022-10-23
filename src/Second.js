00c62cfa1aeed8280bf1ac6418a864e2a636abd9
jeongkyojun <kyojun2002@naver.com>
2022-10-23 20:59:56 +0900

    react 자습서 - 틱택토게임

---
+++ src/Second.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Second() {
+  return (
+    <div>
+      <h1>Do you know me?</h1>
+      <h2>My name is what?</h2>
+    </div>
+  );
+}
+
+export default Second;
---
+++ src/components/layout/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main(props) {
+  return (
+    <>
+      <h1>Main 입니다.</h1>
+    </>
+  );
+}
+
+export default Main;
